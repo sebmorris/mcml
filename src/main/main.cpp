92f0c175ac65d8476a5de040b45f8e62d6dd2c09
Seb Morris <seb.morris@btinternet.com>
2022-12-04 21:41:59 +0000

    added argument support to main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,18 +5,24 @@ using std::cout;
 using std::vector;
 
 #include <cmath>
+#include <cstring>
 
 #include "../layer/layer.hpp"
 #include "../material/material.hpp"
 #include "../simulation/simulation.hpp"
 
-int main() {
+void verificationModels() {
+    cout << "Running verification models" << std::endl;
+
     vector<Layer> layers{Layer(0.9, 1.4, 0.04, 20, 10), Layer(0.9, 1.4, 0.001, 0.1, 2), Layer(0.9, 1.4, 0.025, 25, 4), Layer(0.9, 1.4, 0.005, 60)};
     
     Material material(layers);
     cout << material;
 
-    Simulation simulation(material);
+    vector<double> trackedDistances{10, 20, 30};
+    double trackingInterval = 0.5;
+
+    Simulation simulation(material, trackedDistances, trackingInterval);
 
     int N = 1e6;
     for (int i = 0; i < N; i++) {
@@ -25,15 +31,14 @@ int main() {
         }
         simulation.nextPhoton();
     }
-    cout << std::endl << "Done, " << simulation.photonsLaunched << " photons used" << std::endl;
+    cout << std::endl << "Done, " << simulation.launchedPhotons() << " photons used" << std::endl;
+}
 
-    for (auto e : simulation.results.reflectance) {
-        cout << e << " ";
+int main(int argc, char** argv) {
+    if (argc == 2 && strcmp(argv[1], "verification") == 0) {
+        verificationModels();
+        return 0;
     }
-    cout << std::endl;
 
-    cout << "Reflectance overflow " << simulation.results.reflectanceOverflow << std::endl;
-    cout << "Absorption overflow " << simulation.results.absorptionOverflow << std::endl;
-
-    simulation.results.save(simulation.photonsLaunched);
+    cout << "No program matches" << std::endl;
 }
