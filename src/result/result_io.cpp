3d070b0fed76024f640b36bebc7cf3fd05adba74
Sebastian Morris <seb.morris@btinternet.com>
2022-12-06 20:26:16 +0000

    fixed outputting significant figs

--- src/result/result_io.cpp
+++ src/result/result_io.cpp
@@ -5,27 +5,21 @@ using std::to_string;
 using std::vector;
 using std::ostream;
 
-string csvRowString(const RadialTracker::row& row) {
-    if (row.size() == 0) return "";
+ostream& csvRowString(ostream& os, const RadialTracker::row& row) {
+    if (row.size() == 0) return os;
 
     // have to treat either the first or last element specially
-    string result(to_string(row[0]));
+    os << row[0];
     for (auto it = row.begin() + 1; it != row.end(); it++) {
-        result += ", " + to_string(*it);
+        os << ", " << *it;
     }
-
-    return result;
-}
-
-ostream& csvRowString(ostream& os, const RadialTracker::row& row) {
-    os << csvRowString(row);
 
     return os;
 }
 
 ostream& csvGridString(ostream& os, const BulkTracker::grid& grid) {
     for (auto i : grid) {
-        os << csvRowString(i) << "\n";
+        csvRowString(os, i) << "\n";
     }
 
     return os;
