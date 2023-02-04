7dcfba737bcacb2fbc84b5888dd0ce258b49d53c
Sebastian Morris <seb.morris@btinternet.com>
2023-02-04 13:12:01 +0000

    updated random implementation for multi-threading

--- src/random/random.cpp
+++ src/random/random.cpp
@@ -2,24 +2,39 @@
 
 using std::cout;
 
-double sim_random() {
-    static std::random_device device;
-    static std::default_random_engine generator(device());
+RandomInternals::RandomInternals(std::uint_fast32_t seed) : gen(seed) {  }
 
-    static std::uniform_real_distribution<double> distribution{0.0, 1.0};
+Random::Random(std::uint_fast32_t seed) : internals_(std::make_shared<RandomInternals>(seed)) {  }
 
+double Random::operator()() {
     double rand;
 
-    do rand = distribution(generator);
+    do rand = internals_->dist(internals_->gen);
     while (rand <= 0); // ensures rand != 0
-
-    if (rand > 1) {
-        cout << "should never reach" << std::endl;
-    }
 
     return rand;
 }
 
-double sim_random_between(double lower, double upper) {
-    return (upper - lower) * sim_random();
+double Random::random_between(double lower, double upper) {
+    return lower + (upper - lower) * (*this)();
+}
+
+std::vector<std::uint_fast32_t> manySeeds(std::size_t n) {
+    std::vector<std::uint_fast32_t> seeds(n);
+    std::seed_seq seq{std::random_device()()};
+
+    seq.generate(seeds.begin(), seeds.end());
+
+    return seeds;
+}
+
+std::vector<Random> manyRandoms(std::vector<std::uint_fast32_t> seeds) {
+    std::vector<Random> randoms;
+    randoms.reserve(seeds.size());
+
+    for (auto seed : seeds) {
+        randoms.emplace_back(seed);
+    }
+
+    return randoms;
 }
--- src/random/random.hpp
+++ src/random/random.hpp
@@ -1,9 +1,31 @@
 #ifndef RANDOM_H
+#define RANDOM_H
 
 #include <random>
 #include <iostream>
+#include <memory>
+#include <cstdint>
+#include <vector>
 
-double sim_random();
-double sim_random_between(double, double);
+struct RandomInternals {
+    std::default_random_engine gen;
+    std::uniform_real_distribution<double> dist{0, 1};
+
+    RandomInternals(std::uint_fast32_t);
+};
+
+class Random {
+    private:
+        mutable std::shared_ptr<RandomInternals> internals_;
+    public:
+        Random() = delete;
+        Random(std::uint_fast32_t);
+
+        double operator()();
+        double random_between(double, double);
+};
+
+std::vector<std::uint_fast32_t> manySeeds(std::size_t);
+std::vector<Random> manyRandoms(std::vector<std::uint_fast32_t>);
 
 #endif
