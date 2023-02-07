1b531fe76d9689df6fe97c7d100ac8211d8c5a2b
Sebastian Morris <seb.morris@btinternet.com>
2023-02-07 18:08:32 +0000

    added a couple new constants

--- src/constants/constants.hpp
+++ src/constants/constants.hpp
@@ -2,9 +2,12 @@
 #define CONSTANTS_H
 
 #include <limits>
+#include <cstdint>
 
 constexpr double pi = 3.14159265358979323846;
 constexpr double inf = std::numeric_limits<double>::infinity();
-constexpr double SIMULATIONS_PER_THREAD = 2;
+constexpr uint64_t SIMULATIONS_PER_THREAD = 1000;
+constexpr uint64_t PHOTONS_PER_SIMULATION = 1e6;
+constexpr uint64_t PHOTONS_PER_REPORT = 1e4;
 
 #endif
