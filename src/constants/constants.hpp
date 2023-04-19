14b90d49f6fc464e0499be81aa1a60f210600222
Sebastian Morris <seb.morris@btinternet.com>
2023-04-19 21:34:58 +0100

    more appropriate constants for random-parameter-simulations

--- src/constants/constants.hpp
+++ src/constants/constants.hpp
@@ -6,8 +6,8 @@
 
 constexpr double pi = 3.14159265358979323846;
 constexpr double inf = std::numeric_limits<double>::infinity();
-constexpr uint64_t SIMULATIONS_PER_THREAD = 1000;
-constexpr uint64_t PHOTONS_PER_SIMULATION = 1e7;
-constexpr uint64_t PHOTONS_PER_REPORT = 1e5;
+constexpr uint64_t SIMULATIONS_PER_THREAD = 1;
+constexpr uint64_t PHOTONS_PER_SIMULATION = 2e6;
+constexpr uint64_t PHOTONS_PER_REPORT = 1e4;
 
 #endif
