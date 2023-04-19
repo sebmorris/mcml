72f3a672a4ca6edb80039a4753f761c61acc952d
Sebastian Morris <seb.morris@btinternet.com>
2023-04-19 21:34:40 +0100

    different threading strategy for random-parameter-simulations

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -12,6 +12,7 @@ using std::round;
 
 #include <random>
 #include <thread>
+#include <functional>
 
 #include "../layer/layer.hpp"
 #include "../material/material.hpp"
@@ -22,6 +23,10 @@ using std::round;
 #include "../random/random.hpp"
 #include "../constants/constants.hpp"
 #include "../parameter_simulation/parameter_simulation.hpp"
+
+void run(Simulation& sim) {
+    runSimulation(sim);
+}
 
 void verificationModels() {
     cout << "Running verification models" << std::endl;
@@ -44,10 +49,17 @@ void verificationModels() {
     //layers.emplace_back(BaseLayerOptions(1, 0.001, 5), 0.1, 0.9);
     //layers.emplace_back(BaseLayerOptions(0.5, 0.005), 50, 0.8);
 
+    /*
+        prior test speed
+    */
+    layers.emplace_back(BaseLayerOptions(1.4, 0.01, 10), 500, 0.9);
+    layers.emplace_back(BaseLayerOptions(1.4, 0, 5), 1, 0.9);
+    layers.emplace_back(BaseLayerOptions(1.4, 0.01), 500, 0.9);
+
     std::vector<Random> randoms = manyRandoms(manySeeds(1));
 
-    //Material material(layers, 1);
-    Material material = sampleMaterial(randoms[0]);
+    Material material(layers, 1.4);
+    //Material material = sampleMaterial(randoms[0]);
     cout << material;
 
     for (auto b : material.boundaries_) {
@@ -97,13 +109,26 @@ void randomSampling(Random random, Recording recorder) {
     }
 } 
 
-void randomParameterSampling(Random random, Recording recorder) {
-    for (int i = 0; i < SIMULATIONS_PER_THREAD; i++) {
-        ParameterSimulationMaterialOptions options = sampleParameterMaterials(random, 1);
-        ParameterSimulation sims(options.material, options.layers, random);
+void randomParameterSampling(Recording recorder) {
+    static unsigned int noThreads = 81;
 
-        runSimulation(sims);
-        cout << "Done the " << i << "th simulation on one thread" << std::endl;
+    vector<Random> randoms = manyRandoms(manySeeds(noThreads));
+
+    for (int i = 0; i < SIMULATIONS_PER_THREAD; i++) {
+        ParameterSimulationMaterialOptions options = sampleParameterMaterials(randoms[0], 1);
+        ParameterSimulation sims(options.material, options.layers, randoms);
+
+        vector<std::thread> threads;
+        threads.reserve(noThreads);
+
+        cout << "Spawning " << noThreads << " threads" << std::endl;
+        for (auto& sim : sims.simulations_) {
+            threads.emplace_back(run, std::ref(sim));
+        }
+
+        for (auto& thread : threads) {
+            thread.join();
+        }
 
         recorder.save(sims);
 
@@ -155,23 +180,8 @@ int main(int argc, char** argv) {
             return -1;
         }
 
-        unsigned int noThreads = std::thread::hardware_concurrency();
-        //unsigned int noThreads = 1;
-
-        vector<Random> randoms = manyRandoms(manySeeds(noThreads));
-
         Recording recorder(argv[2]);
-        vector<std::thread> threads;
-        threads.reserve(randoms.size());
-
-        cout << "Spawning " << noThreads << " threads" << std::endl;
-        for (auto random : randoms) {
-            threads.emplace_back(randomParameterSampling, random, recorder);
-        }
-
-        for (auto& thread : threads) {
-            thread.join();
-        }
+        randomParameterSampling(recorder);
 
         return 0;
     }
